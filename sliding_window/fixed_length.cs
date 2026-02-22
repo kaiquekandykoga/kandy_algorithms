18787b946334d35afcf7a90b9f52ec2d81fcad23
Kaíque Kandy Koga <kaiquekandykoga@gmail.com>
2026-02-23 12:37:38 +1300

    add sliding_window/fixed_length.cs

---
+++ sliding_window/fixed_length.cs
@@ -0,0 +1,9 @@
+static void Call(int[] nums, int length)
+{
+    Console.WriteLine("TODO: Implement fixed length encoding");
+}
+
+int[] nums = { 2, 1, 5, 1, 3, 2 };
+int length = 3;
+Call(nums, length); // Output: 9
+
